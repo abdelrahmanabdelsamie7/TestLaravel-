c5d2a3079f5927066a5ba54e2da2a44e0a6ca6e7
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2025-01-19 03:59:20 +0200

    s push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MediCare</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body>
-    <h5>احلي مساء عليك يا ابو ماهر</h5>
+<body class="bg-secondary d-flex justify-content-between align-items-center" style="height: 100vh">
+    <div class="container my-5 text-center ">
+        <h1 class="display-1 text-info fw-bold">MediCare <br> Comming Soon </h1>
+    </div>
 </body>
 
 </html>
